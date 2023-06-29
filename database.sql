3045f4cb4eb5cf37e50e3696fb3b7e1c959554dd
Diebut <guevaradiego_2011@hotmail.com>
2023-06-29 18:54:37 -0300

    se agrega otra tabla users y se inserta datos en la misma

--- database.sql
+++ database.sql
@@ -7,3 +7,10 @@ create TABLE libros(
 );
 
 insert into libros VALUES(2, 'Digital Manager', 'frey Green'),(3, 'Developers Java', 'Sofia Hoss');
+
+create Table users(
+    username text,
+    password text
+);
+
+insert into users VALUES('joe', 'joe123'),('leo', 'leo123'),('sol', 'sol123');
